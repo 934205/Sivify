e9f1393ea3a90eb98a3894797738e9ac394cf3f9
934205 <nadesanjayarani4@gmail.com>
2025-07-31 15:30:57 +0530

    Update: fixed backend db credentials and improved UI

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ const uplod=multer({memoryStorage,limits:{fileSize: 20 * 1024 * 1024 }})
 app.use(express.json({limit:"50mb"}))
 app.use(cors(
     {
-    origin: ["http://localhost:5173","http://192.168.1.5:5173"], 
+    origin: ["http://localhost:5173","http://192.168.1.61:5173"], 
     credentials: true 
     }
 ))
