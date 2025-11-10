06b4945008258291aa4d93761f94ac92181ea947
934205 <github_mail>
2025-11-10 14:43:30 +0530

    latest

--- backend/mail/transporter.js
+++ backend/mail/transporter.js
@@ -1,15 +1,51 @@
-const nodemailer=require("nodemailer")
-require("dotenv").config()
+// const nodemailer=require("nodemailer")
+// require("dotenv").config()
 
-const transport=nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-      user:process.env.EMAIL_USER,
-      pass:process.env.EMAIL_PASS
-    },
-    tls:{
+// const transport=nodemailer.createTransport({
+//     service:"gmail",
+//     auth:{
+//       user:process.env.EMAIL_USER,
+//       pass:process.env.EMAIL_PASS
+//     },
+//     tls:{
+//       rejectUnauthorized:false
+//     }
+//   })
+
+//   module.exports=transport
+
+
+const nodemailer = require("nodemailer");
+
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls:{
       rejectUnauthorized:false
     }
-  })
+});
 
-  module.exports=transport
+module.exports = transport;
+
+
+// import nodemailer from "nodemailer";
+
+// export const transport = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   service:"gmail",
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+//   tls:{
+//       rejectUnauthorized:false
+//     }
+// });
+
+//   module.exports=transport
+
