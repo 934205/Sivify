7c980e88078fab60ec679cae58bcaf0c27740ff0
934205 <github_mail>
2025-11-10 15:02:56 +0530

    latest

--- backend/mail/transporter.js
+++ backend/mail/transporter.js
@@ -18,17 +18,20 @@
 const nodemailer = require("nodemailer");
 
 const transport = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587, // use 587 instead of 465
+  secure: false, // false for TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls:{
-      rejectUnauthorized:false
-    }
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = transport;
+
 
 
 // import nodemailer from "nodemailer";
